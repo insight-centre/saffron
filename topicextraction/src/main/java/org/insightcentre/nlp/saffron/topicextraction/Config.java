6a4486eed54bf9908d6ed6275405bd6857edf597
John P. McCrae <john@mccr.ae>
2016-11-03 15:02:05 +0000

    Remove old modules

--- topicextraction/src/main/java/org/insightcentre/nlp/saffron/topicextraction/Config.java
+++
@@ -1,9 +0,0 @@
-package org.insightcentre.nlp.saffron.topicextraction;
-
-public class Config {
-
-    public static final Integer maxTokens = 5;
-    public static final Integer minTokens = 1;
-    public static final Integer maxTopicsPerDocument = null;
-
-}
