d70cd422b4ea1fc45d70f770b85d8a9d92893676
John P. McCrae <john@mccr.ae>
2017-04-19 11:16:50 +0100

    Implement ATR4S bridge in Java

---
+++ atr4s/src/main/java/org/insightcentre/nlp/saffron/atr4s/Main.java
@@ -0,0 +1,93 @@
+package org.insightcentre.nlp.saffron.atr4s;
+
+import static com.sun.jersey.server.wadl.generators.resourcedoc.xhtml.Elements.val;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import org.insightcentre.nlp.saffron.data.Corpus;
+import org.insightcentre.nlp.saffron.data.Topic;
+import org.insightcentre.nlp.saffron.data.index.DocumentSearcher;
+import org.insightcentre.nlp.saffron.data.index.DocumentSearcherFactory;
+import parquet.org.codehaus.jackson.map.ObjectMapper;
+import static scala.collection.JavaConversions.asJavaIterable;
+
+/**
+ * Main point for the ATR4S-based extract-topics command
+ *
+ * @author John McCrae <john@mccr.ae>
+ */
+public class Main {
+
+    public static void badOptions(OptionParser p, String message) throws IOException {
+        System.err.println("Error: " + message);
+        p.printHelpOn(System.err);
+        System.exit(-1);
+    }
+
+    public static void main(String[] args) {
+        try {
+            OptionParser p = new OptionParser() {
+                {
+                    accepts("c", "The configuration to use").withRequiredArg().ofType(File.class);
+                    accepts("x", "The text corpus to use").withRequiredArg().ofType(File.class);
+                    accepts("t", "Where to write the topics to").withRequiredArg().ofType(File.class);
+                    accepts("o", "Where to write the document-topic mapping to").withRequiredArg().ofType(File.class);
+                }
+            };
+
+            final OptionSet os;
+            try {
+                os = p.parse(args);
+            } catch (Throwable x) {
+                badOptions(p, x.getMessage());
+                return;
+            }
+            File configuration = (File) os.valueOf("c");
+            if (configuration == null || !configuration.exists()) {
+                badOptions(p, "Configuration does not exist");
+            }
+            File corpusFile = (File) os.valueOf("x");
+            if (corpusFile == null || !corpusFile.exists()) {
+                badOptions(p, "Corpus does not exist");
+            }
+            File outputTopics = (File) os.valueOf("t");
+            if (outputTopics == null) {
+                badOptions(p, "Output not specified");
+            }
+            File outputDocTopics = (File) os.valueOf("o");
+            if (outputDocTopics == null) {
+                badOptions(p, "Output not specified");
+            }
+            ObjectMapper mapper = new ObjectMapper();
+            // Read configuration
+            Configuration config = mapper.readValue(configuration, Configuration.class);
+            Corpus corpus = mapper.readValue(corpusFile, Corpus.class);
+            DocumentSearcher searcher = DocumentSearcherFactory.loadSearcher(corpus);
+
+            TopicExtraction extractor = new TopicExtraction(config);
+
+            TopicExtraction.Result res = extractor.extractTopics(searcher);
+
+            mapper.writerWithDefaultPrettyPrinter().writeValue(outputTopics, res.topics);
+            mapper.writerWithDefaultPrettyPrinter().writeValue(outputDocTopics, res.docTopics);
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+    public static class Configuration {
+
+        public double threshold = 0.5;
+        public int ngramMin = 1;
+        public int ngramMax = 4;
+        public int minTermFreq = 2;
+        public String method = "one"; // "one" || "voting" || "puatr"
+        public List<String> features = java.util.Arrays.asList("weirdness");
+        public String corpus;
+
+    }
+}
---
+++ atr4s/src/main/java/org/insightcentre/nlp/saffron/atr4s/TopicExtraction.java
@@ -0,0 +1,169 @@
+package org.insightcentre.nlp.saffron.atr4s;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import org.insightcentre.nlp.saffron.data.Document;
+import org.insightcentre.nlp.saffron.data.Topic;
+import org.insightcentre.nlp.saffron.data.connections.DocumentTopic;
+import org.insightcentre.nlp.saffron.data.index.DocumentSearcher;
+import org.insightcentre.nlp.saffron.data.index.SearchException;
+import ru.ispras.atr.candidates.TCCConfig;
+import ru.ispras.atr.candidates.TermCandidatesCollector;
+import ru.ispras.atr.candidates.TermOccurrencesCollectorConfig;
+import ru.ispras.atr.datamodel.DSDataset;
+import ru.ispras.atr.datamodel.TermCandidate;
+import ru.ispras.atr.datamodel.TermOccurrence;
+import ru.ispras.atr.features.FeatureConfig;
+import ru.ispras.atr.features.refcorpus.ReferenceCorpusConfig;
+import ru.ispras.atr.features.refcorpus.Weirdness;
+import ru.ispras.atr.preprocess.EmoryNLPPreprocessorConfig;
+import ru.ispras.atr.preprocess.NLPPreprocessor;
+import ru.ispras.atr.rank.OneFeatureTCWeighterConfig;
+import ru.ispras.atr.rank.TermCandidatesWeighter;
+import scala.Tuple2;
+import scala.collection.JavaConversions;
+import scala.collection.Seq;
+
+/**
+ * Extract Topics using ATR4S
+ * @author John McCrae <john@mccr.ae>
+ */
+public class TopicExtraction {
+    private final Main.Configuration config;
+    private final NLPPreprocessor nlpPreprocessor;
+    private final TermCandidatesCollector candidatesCollector;
+    private final TermCandidatesWeighter candidatesWeighter;
+    private final double threshold;
+
+    public TopicExtraction(Main.Configuration config) {
+        this.config = config;
+        this.nlpPreprocessor =  EmoryNLPPreprocessorConfig.make().build();
+        this.candidatesCollector = TCCConfig.make(range(config.ngramMin, config.ngramMax), 
+            config.minTermFreq, TermOccurrencesCollectorConfig.make()).build();
+        if(config.method.equals("one")) {
+            this.candidatesWeighter = new OneFeatureTCWeighterConfig(mkFeats(config).get(0)).build();
+        } else {
+            throw new IllegalArgumentException("Unknown method: " + config.method);
+        }
+        this.threshold = config.threshold;
+    }
+    
+    private static List<FeatureConfig> mkFeats(Main.Configuration config) {
+        List<FeatureConfig> feats = new ArrayList<>();
+        for(String name : config.features) {
+            if(name.equals("weirdness")) {
+                feats.add(new Weirdness(ReferenceCorpusConfig.apply(config.corpus, 1e-20)));
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+        return feats;
+        
+    
+    }
+    
+    public static class Result {
+        public Set<Topic> topics;
+        public List<DocumentTopic> docTopics;      
+
+        @Override
+        public String toString() {
+            return "Result{" + "topics=" + topics + ", docTopics=" + docTopics + '}';
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = 53 * hash + Objects.hashCode(this.topics);
+            hash = 53 * hash + Objects.hashCode(this.docTopics);
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final Result other = (Result) obj;
+            if (!Objects.equals(this.topics, other.topics)) {
+                return false;
+            }
+            if (!Objects.equals(this.docTopics, other.docTopics)) {
+                return false;
+            }
+            return true;
+        }
+        
+        
+    }
+    
+    public Result extractTopics(DocumentSearcher searcher) {
+        Result result = new Result();
+        result.topics = new HashSet<>();
+        result.docTopics = new ArrayList<>();
+        try {
+            List<Tuple2<String, String>> docs = new ArrayList<>();
+            for(Document doc : searcher.allDocuments()) {
+                docs.add(new Tuple2<>(doc.getId(), doc.getContents()));
+            }
+            DSDataset dataset = nlpPreprocessor.preprocess(JavaConversions.iterableAsScalaIterable(docs).toSeq());
+            Seq<TermCandidate> candidates = candidatesCollector.collect(dataset);
+            Map<String,TermCandidate> candmap = new HashMap<>();
+            for(TermCandidate tc : JavaConversions.asJavaIterable(candidates)) {
+                candmap.put(tc.canonicalRepr(), tc);
+            }
+            Iterable<Tuple2<String,Object>> sortedTerms = JavaConversions.asJavaIterable(
+                    candidatesWeighter.weightAndSort(candidates, dataset));
+            for(Tuple2<String,Object> term : sortedTerms) {
+                final Double score = (Double)term._2;
+                if(score > threshold) {
+                    TermCandidate tc = candmap.get(term._1);
+                    Seq<String> lemmas = tc.lemmas();
+                    Map<String,Integer> docOccurs = new HashMap<>();
+                    int occurrences = 0;
+                    for(TermOccurrence to : JavaConversions.asJavaIterable(tc.occurrences())) {
+                        if(docOccurs.containsKey(to.docName()))
+                            docOccurs.put(to.docName(), docOccurs.get(to.docName()) + 1);
+                        else
+                            docOccurs.put(to.docName(), 1);
+                        occurrences++;
+                    }
+                    String lemma1 = lemmas.apply(0).toLowerCase();
+                    List<Topic.MorphologicalVariation> morphVars = new ArrayList<>();
+                    for(String lemma : JavaConversions.asJavaIterable(lemmas)) {
+                        morphVars.add(new Topic.MorphologicalVariation(lemma));
+                    }
+                    
+                    result.topics.add(new Topic(lemma1, occurrences, docOccurs.size(), score, morphVars));
+                    for(Map.Entry<String, Integer> docOcc : docOccurs.entrySet()) {
+                        result.docTopics.add(new DocumentTopic(docOcc.getKey(), lemma1, docOcc.getValue(), null, null));
+                    }
+                }
+            }
+            
+        } catch(SearchException x) {
+            throw new RuntimeException(x);
+        }
+        return result;
+    }
+    
+    private List<Object> range(int i, int j) {
+        List<Object> ints = new ArrayList<>();
+        for(int k = i; k < j; k++) {
+            ints.add(k);
+        }
+        return ints;
+    }
+    
+}
