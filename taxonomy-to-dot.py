805e46f81cb3bf0d8cbc56c79b4743eacee547f2
John P. McCrae <john@mccr.ae>
2017-07-12 11:08:45 +0100

    taxonomy-to-dot now uses weights

--- taxonomy-to-dot.py
+++ taxonomy-to-dot.py
@@ -1,12 +1,12 @@
 import sys
 import json
 
-def gen_label(taxo):
+def gen_label(taxo, topic_wts):
     if "root" in taxo:
-        print("  \"" + taxo["root"] + "\";")
+        print("  \"%s\" [ weight=%.4f ];" % (taxo["root"], topic_wts[taxo["root"]]))
     if "children" in taxo:
         for child in taxo["children"]:
-            gen_label(child)
+            gen_label(child, topic_wts)
 
 def gen_link(taxo):
     if "root" in taxo and "children" in taxo:
@@ -18,10 +18,12 @@ def gen_link(taxo):
 
 def main(args):
     taxonomy = json.load(open(args[0]))
+    topics = json.load(open(args[1]))
+    topic_wts = { x["topic_string"]: x["score"] for x in topics }
     
     print("digraph G {")
     
-    gen_label(taxonomy)
+    gen_label(taxonomy, topic_wts)
 
     gen_link(taxonomy)
 
