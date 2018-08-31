9cd02ffc9a14ced7ea78c3c94d09e58a729f4de0
John P. McCrae <john@mccr.ae>
2018-08-31 11:46:49 +0100

    Fix some minor bugs

--- documentindex/src/main/java/org/insightcentre/nlp/saffron/documentindex/lucene/LuceneIndexer.java
+++ documentindex/src/main/java/org/insightcentre/nlp/saffron/documentindex/lucene/LuceneIndexer.java
@@ -1,8 +1,7 @@
 /**
- * 
+ *
  */
 package org.insightcentre.nlp.saffron.documentindex.lucene;
-
 
 import java.io.Closeable;
 import java.io.IOException;
@@ -17,41 +16,42 @@ import org.insightcentre.nlp.saffron.documentindex.IndexingException;
 
 public class LuceneIndexer implements DocumentIndexer, Closeable {
 
-	private IndexWriter indexWriter;
+    private IndexWriter indexWriter;
 
-	public LuceneIndexer(Directory directory, Analyzer analyzer) throws IndexingException {
-		super();
-		try {
-			IndexWriterConfig config = new IndexWriterConfig(DocumentSearcherFactory.LUCENE_VERSION, analyzer);
-			this.indexWriter = new IndexWriter(directory, config);
-		} catch (IOException e) {
-			throw new IndexingException(e.getMessage(), e);
-		}
-	}
+    public LuceneIndexer(Directory directory, Analyzer analyzer) throws IndexingException {
+        super();
+        try {
+            IndexWriterConfig config = new IndexWriterConfig(DocumentSearcherFactory.LUCENE_VERSION, analyzer);
+            this.indexWriter = new IndexWriter(directory, config);
+        } catch (IOException e) {
+            throw new IndexingException(e.getMessage(), e);
+        }
+    }
 
     @Override
-	public void indexDoc(org.insightcentre.nlp.saffron.data.Document doc, String text) {
-		try {
-            if(doc.id == null)
+    public void indexDoc(org.insightcentre.nlp.saffron.data.Document doc, String text) {
+        try {
+            if (doc.id == null) {
                 throw new IllegalArgumentException("Error reading " + doc.file);
-			indexWriter.addDocument(LuceneDocument.makeDocument(doc.id, text, doc.url, doc.authors, doc.name, doc.file.toFile(), doc.mimeType, doc.metadata));
-		} catch (IOException e) {
-			throw new RuntimeException(e.getMessage(), e);
-		}
-	}
+            }
+            indexWriter.addDocument(LuceneDocument.makeDocument(doc.id, text, doc.url, doc.authors, doc.name, doc.file == null ? null : doc.file.toFile(), doc.mimeType, doc.metadata));
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
 
-	@Override
-	public void commit() {
-		try {
-			indexWriter.commit();
-		} catch (IOException e) {
-			throw new RuntimeException(e.getMessage(), e);
-		}
-	}
-	
-	@Override
-	public void close() throws IOException {
-		indexWriter.commit(); //Just in case one forgets!
-		indexWriter.close();
-	}
+    @Override
+    public void commit() {
+        try {
+            indexWriter.commit();
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        indexWriter.commit(); //Just in case one forgets!
+        indexWriter.close();
+    }
 }
