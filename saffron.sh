f494699ac7c4a79c2894947c3b8d8aa8d85cc26d
John P. McCrae <john@mccr.ae>
2018-11-29 15:45:23 +0000

    Fix saffron.sh for Json files

--- saffron.sh
+++ saffron.sh
@@ -35,17 +35,12 @@ fi
 echo "########################################"
 echo "## Step 1:Indexing corpus             ##"
 echo "########################################"
-if [[ "$1" == *.json ]]
-then
-    CORPUS=$1
-    $DIR/index-corpus -c $CORPUS -i $OUTPUT/index || die "Indexing failed"
-    cp $1 $OUTPUT/corpus-unconsolidated.json
-elif [ -d $1 ] || [[ "$1" == *.zip ]] || [[ "$1" == *.tar.gz ]] || [[ "$1" == *.tgz ]]
+if [[ "$1" == *.json ]] || [ -d $1 ] || [[ "$1" == *.zip ]] || [[ "$1" == *.tar.gz ]] || [[ "$1" == *.tgz ]]
 then
     CORPUS=$OUTPUT/corpus-unconsolidated.json
     $DIR/index-corpus -c $1 -i $OUTPUT/index -o $CORPUS || die "Indexing failed"
 else
-    die "$1 should be Json or a Folder of text files"
+    die "$1 should be Json document, archive or a folder of text files"
 fi
 
 echo "########################################"
