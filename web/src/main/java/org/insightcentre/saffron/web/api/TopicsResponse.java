cac3f22b42a1aaefba6363685d12a4e65f89e4da
Andy Donald <andy.donald@insight-centre.org>
2019-05-01 16:45:46 +0100

    Upload New File

---
+++ web/src/main/java/org/insightcentre/saffron/web/api/TopicsResponse.java
@@ -0,0 +1,24 @@
+package org.insightcentre.saffron.web.api;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+@XmlRootElement()
+public class TopicsResponse extends BaseResponse {
+
+
+    List<TopicResponse> topicsList = new ArrayList<>();
+
+    public List<TopicResponse> getTopics() {
+        return this.topicsList;
+    }
+
+    public void setTopics(TopicResponse topicsList) {
+        this.topicsList.add(topicsList);
+    }
+}
+
