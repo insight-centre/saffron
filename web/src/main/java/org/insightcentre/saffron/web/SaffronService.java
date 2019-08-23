a5de4ac34256318aa9f08dd122a81a194142f7bd
Bianca Pereira <bianca.pereira@insight-centre.org>
2019-08-23 14:07:02 +0100

    Error handling for Saffron service update topics

--- web/src/main/java/org/insightcentre/saffron/web/SaffronService.java
+++ web/src/main/java/org/insightcentre/saffron/web/SaffronService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.insightcentre.nlp.saffron.data.Status;
 import org.insightcentre.nlp.saffron.data.Taxonomy;
 import org.insightcentre.nlp.saffron.data.Topic;
+import org.insightcentre.saffron.web.exceptions.InvalidValueException;
 
 /**
  * Service to connect with other Saffron components and
@@ -28,9 +29,20 @@ public class SaffronService {
 	 * @param topics - the topics to be modified
 	 */
 	public void updateTopicStatus(String taxonomyId, List<Topic> topics) {
+		RuntimeException agException = null;
+		
     	for(Topic topic: topics){
-			updateTopicStatus(taxonomyId,topic);
-		}		
+    		try {
+    			updateTopicStatus(taxonomyId,topic);
+    		} catch (Exception e) {
+    			if (agException == null)
+    				agException = new RuntimeException("some topics were not updated");
+    			agException.addSuppressed(e);
+    		}
+		}
+    	
+    	if (agException != null)
+    		throw agException;
 	}
 
 	/**
@@ -45,15 +57,35 @@ public class SaffronService {
 		 * 2 - If new status = "rejected" then
 		 * 3 - remove the topic from the taxonomy and update the taxonomy in the database
 		 */
+		if (taxonomyId == "") {
+			InvalidValueException exception = new InvalidValueException("The taxonomy id cannot be empty");
+			exception.addParameterValue("taxonomyId", "");
+			throw exception;
+		}		
+		if (topic.topicString == null || topic.topicString.equals("")) {
+			InvalidValueException exception = new InvalidValueException("The topic string cannot be empty or null");
+			exception.addParameterValue("topic.string", topic.topicString);
+			throw exception;
+		}
+		if (topic.status == null) {
+			InvalidValueException exception = new InvalidValueException("The topic status cannot be null");
+			exception.addParameterValue("topic.status", topic.status);
+			throw exception;
+		}
 		
 		// 1 - Change topic status in the database.
-		dataSource.updateTopic(taxonomyId, topic.topicString, topic.status.toString());
+		boolean topicUpdated = dataSource.updateTopic(taxonomyId, topic.topicString, topic.status.toString());
+		if(!topicUpdated)
+			throw new RuntimeException("An error has ocurred when updating the topic in the database.");
 		
 		//2 - If new status = "rejected" then
 		if (topic.status.equals(Status.rejected)) {
 			Taxonomy taxonomy = dataSource.getTaxonomy(taxonomyId);
 			taxonomy = taxonomy.removeChild(topic.topicString, taxonomy);
-			dataSource.updateTaxonomy(taxonomyId, taxonomy);
+			boolean taxonomyUpdated = dataSource.updateTaxonomy(taxonomyId, taxonomy);
+			if(!taxonomyUpdated)
+				throw new RuntimeException("An error has ocurred when updating the taxonomy in the database.");
+				//TODO It should revert the topic update in this case
 		}	
 	}
 }
