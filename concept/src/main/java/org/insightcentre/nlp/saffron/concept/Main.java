0fc6663a75d6f694caadc64964575d3588e2c113
Bianca Pereira <bianca.pereira@insight-centre.org>
2019-11-26 14:39:38 +0000

    Create concept consolidation Maven module #248

---
+++ concept/src/main/java/org/insightcentre/nlp/saffron/concept/Main.java
@@ -0,0 +1,10 @@
+package org.insightcentre.nlp.saffron.concept;
+
+import org.apache.commons.lang3.NotImplementedException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		throw new NotImplementedException("Concept Consolidation not implemented");
+	}
+}
