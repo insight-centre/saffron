cfe25ca7d57cf5c8b9c2099f969e80f5bfd6a7b2
Bianca Pereira <bianca.pereira@insight-centre.org>
2019-11-26 14:28:48 +0000

    Fixing merge issues

--- web/src/main/java/org/insightcentre/saffron/web/mongodb/MongoDBHandler.java
+++ web/src/main/java/org/insightcentre/saffron/web/mongodb/MongoDBHandler.java
@@ -87,6 +87,13 @@ public class MongoDBHandler extends HttpServlet implements SaffronDataSource {
     final MongoCollection termsCollection;
     final MongoCollection termsCorrespondenceCollection;
     final MongoCollection termsExtractionCollection;
+    
+    final MongoCollection conceptsCollection;
+    final String RUN_IDENTIFIER = "run";
+    final String CONCEPT_IDENTIFIER = "concept_id";
+    final String CONCEPT_PREFERRED_TERM_STRING = "preferred_term";
+    final String CONCEPT_SYNONYM_LIST = "synonyms";
+    
     final MongoCollection authorTermsCollection;
     final MongoCollection termsSimilarityCollection;
     final MongoCollection authorSimilarityCollection;
