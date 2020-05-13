a2583f618bb71fdd58268816fa2335f58a596726
Bianca Pereira <bianca.pereira@insight-centre.org>
2020-05-13 19:31:32 +0100

    Allow creation of Document without content, file or url #306

--- core/src/main/java/org/insightcentre/nlp/saffron/data/Document.java
+++ core/src/main/java/org/insightcentre/nlp/saffron/data/Document.java
@@ -84,7 +84,7 @@ public class Document {
         } else if (url != null) {
             this.contents = new Remote();
         } else {
-            throw new IllegalArgumentException("Please give either document contents or link to file");
+            this.contents = null;
         }
     }
 
