ed42ede5ed3a355174636cdecdbcb0a050d2d56b
John P. McCrae <john@mccr.ae>
2020-11-09 13:30:59 +0000

    Fix error in changing wrong script

--- export-kg.sh
+++ export-kg.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Function to quit on error
-die() { echo "$@" 1>&2 ; exit 1; }
-
-cd $DIR/web
-export MONGO_HOST=localhost
-export MONGO_PORT=27017
-export MONGO_DB_NAME=saffron_test
-mvn -q exec:java@kg-extraction -f pom.xml -Dexec.args="$*"
+mvn -q exec:java -f $DIR/taxonomy/pom.xml -Dexec.mainClass="org.insightcentre.nlp.saffron.taxonomy.extract.KGExtraction" -Dexec.args="$*"
